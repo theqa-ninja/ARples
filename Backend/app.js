36796e48d21319da6a240742dd31068479845fe9
David Tan <dtan@instructure.com>
2019-11-02 11:31:05 -0500

    respond!

--- Backend/app.js
+++ Backend/app.js
@@ -23,6 +23,27 @@ app.get('/test', (request, response) => {
 app.post('/webhook', (req, res) => {
   const VERIFY_TOKEN = "blah";
 
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+
   // // Parse the request body from the POST
   // let body = req.body;
 
@@ -50,8 +71,6 @@ app.post('/webhook', (req, res) => {
   //   console.log('beep 404 on webhook')
   //   res.sendStatus(404);
   // }
-
-  res.status(200).send(`${VERIFY_TOKEN}`)
 
 });
 
