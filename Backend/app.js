ee8ac6b859089e8ca774b5db2199e078dd5c9953
David Tan <dtan@instructure.com>
2019-11-02 12:39:49 -0500

    push again

--- Backend/app.js
+++ Backend/app.js
@@ -115,8 +115,10 @@ function callSendAPI(sender_psid, response) {
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
-    console.log(`res: ${res}`);
-    console.log(`body: ${body}`);
+    console.log(`res`)
+    console.log(res);
+    console.log(`body`)
+    console.log(body)
     if (!err) {
       console.log('message sent!')
     } else {
