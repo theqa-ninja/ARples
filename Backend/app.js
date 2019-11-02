3c15aea7553a32aeb3c3dc7f00a0e2c68a610ba6
David Tan <dtan@instructure.com>
2019-11-02 11:35:31 -0500

    ugh for bad calls

--- Backend/app.js
+++ Backend/app.js
@@ -27,23 +27,23 @@ app.post('/webhook', (req, res) => {
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
-      
+
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-    
+
       // Checks the mode and token sent is correct
       if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        
+
         // Responds with the challenge token from the request
         console.log('WEBHOOK_VERIFIED');
         res.status(200).send(challenge);
-      
+
       } else {
         // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);      
+        res.sendStatus(403);
       }
     }
-
+    res.status(303).send("ugh")
   // // Parse the request body from the POST
   // let body = req.body;
 
