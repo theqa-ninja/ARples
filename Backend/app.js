e3dfb823c68b394669e07087a8dc9cb120ee0485
David Tan <dtan@instructure.com>
2019-11-02 15:22:58 -0500

    adding logging

--- Backend/app.js
+++ Backend/app.js
@@ -181,6 +181,7 @@ function handleMessage(sender_psid, received_message) {
         };
     }
   } else if (received_message.attachments) {
+    console.log(`attachments: ${JSON.stringify(received_message.attachments)}`)
     // Get the URL of the message attachment
     let attachment_url = received_message.attachments[0].payload.url;
     response = {
