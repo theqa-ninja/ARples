e4ad58dd2e541e5bd25e25bbde0f080a396ddeeb
David Tan <dtan@instructure.com>
2019-11-02 12:06:55 -0500

    blah

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
-const port = process.env.PORT || 3000
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(
